35942c14da9b2320bb3896891a1cb33100dbeff9
Koushik-Zzz <Koushik02269@gmail.com>
2025-07-16 01:40:34 +0530

    add: added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/next"
 import { SpeedInsights } from "@vercel/speed-insights/next"
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
@@ -41,6 +42,7 @@ export default function RootLayout({
         <Provider>
         {children}
         <SpeedInsights />
+        
         <Toaster
           position="top-right"
           richColors
@@ -61,6 +63,7 @@ export default function RootLayout({
           }}
         />
         </Provider>
+        <Analytics />
       </body>
     </html>
   );

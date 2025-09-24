5299cf7eb78c95b2dc0076f91bab4fe555231247
Koushik-Zzz <Koushik02269@gmail.com>
2025-09-24 20:37:26 +0530

    fix: linting error

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,18 +9,17 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  {
-    // Ignore all generated files and build outputs
-    ignores: [
-        "lib/generated/", 
-        ".next/",         
-        "node_modules/",  
-        "*.config.js",    
-        "*.config.mjs",   
-      ],
-  },
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+const eslintConfig = [{
+  ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"]
+}, {
+  // Ignore all generated files and build outputs
+  ignores: [
+      "lib/generated/", 
+      ".next/",         
+      "node_modules/",  
+      "*.config.js",    
+      "*.config.mjs",   
+    ],
+}, ...compat.extends("next/core-web-vitals", "next/typescript")];
 
 export default eslintConfig;
